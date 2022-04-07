8ffac9d365dee2e2db67e725c4b44089a50e52fa
Carlos Seguido <carlos-segui-mtnez@hotmail.com>
2022-04-07 13:39:29 +0200

    Add bottom navigation bar first try

--- app/src/main/java/voluntarix/screens/MainActivity.java
+++ app/src/main/java/voluntarix/screens/MainActivity.java
@@ -1,14 +1,39 @@
 package voluntarix.screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment home = new HomeFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, home).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.HomeFragment);
     }
 }
